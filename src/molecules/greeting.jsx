3264dc3939b9640b0fded4d15ea8935d88e91fa3
Nancy <nancy.blanco.suarez@gmail.com>
2024-03-12 12:34:07 -0500

    feat(test):Assigment-3 Testing an Input Field

--- src/molecules/greeting.jsx
+++ src/molecules/greeting.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 
 export default function Greeting() {
+    const [name, setName] = React.useState('');
     const [message, setMessage] = React.useState('Hello, World!');
 
-    const handleClick = () => {
-        setMessage('Hello, User!');
+    const handleChange = (event) => {
+        setName(event.target.value);
     };
+
+    const handleClick = () => {
+        setMessage(`Hello, ${name || 'World'}!`);
+    };
+
     return (
         <div>
-            <p>{message}</p>
-            <button onClick={handleClick}>Change Message</button>
+            <input type="text" placeholder="Enter your name" value={name} onChange={handleChange} data-testid="name-input" />
+            <p data-testid="greeting-message" >{message}</p>
+            <button onClick={handleClick} data-testid="greeting-message" >Change Message</button>
         </div>
     )
 }
